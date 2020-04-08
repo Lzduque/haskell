be6d7b89c77f3dc66a133805b19af1b35ce6408a
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-04-08 10:08:07 -0400

    another way tosolve it with unfoldr

--- DecodeHexBytes.hs
+++ DecodeHexBytes.hs
@@ -11,6 +11,15 @@ decodeCOBS acc (x:xs) = decodeCOBS acc' ys
         (chunk,ys) = splitAt x (x:xs)
         acc' = acc ++ [chunk]
 
+decodeCOBS' :: [Int] -> [[Int]]
+decodeCOBS' = unfoldr go
+    where
+        go [] = Nothing
+        -- go (x:xs) =
+        --     let (chunk, ys) = splitAt x (x:xs)
+        --     in Just splitAt x (x:xs)
+        go (x:xs) = Just $ splitAt x (x:xs)
+
 main = do
     print $ map (\x -> showHex x "") $ decodeHexBytes [0x03, 0x07, 0x09, 0x02, 0x01, 0x01, 0x06, 0x02, 0x03, 0x04, 0x05, 0x06, 0x03, 0x18, 0x22]
     print "[0x00, 0x07, 0x09, 0x00, 0x01, 0x00, 0x00, 0x02, 0x03, 0x04, 0x05, 0x06, 0x00, 0x18, 0x22]"
