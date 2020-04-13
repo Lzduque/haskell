c2b7dc0cdbadc00f92837c66bd8ca657db568540
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-04-13 11:07:07 -0400

    refactor without sort

--- Stray.hs
+++ Stray.hs
@@ -1,13 +1,14 @@
-import Data.Sort (sort)
+import Data.List (group)
 
 stray :: [Int] -> Int
 stray xs
-    | a /= b && a/= c = a
-    | otherwise = b
+    | length filtered == 1 = head $ head filtered
+    | otherwise = head $ head $ drop 1 newXs
     where
-        [a, b, c] = take 3 $ sort xs
-
+        newXs = group xs
+        filtered = filter (\x -> length x == 1) newXs
 
 main = do
     print $ stray [1,1,2] -- 2
     print $ stray [17, 17, 3, 17, 17, 17, 17] -- 3
+    print $ stray [3569,-4614,3569,3569,3569,3569,3569] -- -4614
