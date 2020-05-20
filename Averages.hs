c96742c59b3483c4fa963f3ee32e26a1a4f0a6dd
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-05-20 18:13:27 -0400

    refactor

--- Averages.hs
+++ Averages.hs
@@ -1,15 +1,8 @@
 averages :: Maybe [Double] -> [Double]
-averages xs
-    | length input /= 0 = avrgs input
-    | otherwise = []
-    where
-        fromJust (Just a) = a
-        fromJust Nothing = []
-        input = fromJust xs
-        avrgs [] = []
-        avrgs [x] = []
-        avrgs (x:xs) = average x (head xs) : avrgs xs
-        average x y = (x + y) / 2
+averages Nothing = []
+averages (Just []) = []
+averages (Just [_]) = []
+averages (Just (x:y:xs)) = (x + y) / 2 : averages (Just $ y:xs)
 
 main = do
     print $ averages (Just [2, 2, 2, 2, 2])   -- [2.0, 2.0, 2.0, 2.0]
