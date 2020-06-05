d007f235c4721d03989aef47233d4d60bbee0320
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-05 12:17:00 -0400

    code wars daily exercise

--- FindOdd.hs
+++ FindOdd.hs
@@ -2,10 +2,17 @@
 --   odd number of times. The tests will always
 --   provide such a number, and the list will
 --   always contain at least one element.
-import Data.List (nub,sort)
+-- import Data.List (nub,sort)
+import qualified Data.Map.Strict as Map
 
 findOdd :: [Int] -> Int
-findOdd xs = head [x | x <- xs, odd . length $ filter (== x) xs]
+-- findOdd xs = head [x | x <- xs, odd . length $ filter (== x) xs]
+findOdd xs = undefined
+    where
+        numsCount = foldl (\acc, x -> case ) xs
+        
+
+
 
 main = do
     print $ findOdd [1,2,1,4,2,3,4,2,3] -- 2
