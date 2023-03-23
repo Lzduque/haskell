7d0015c10c8e227d6555b48a65b63bc78d5afa0a
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2023-03-23 14:38:56 -0400

    Update Human Readable Time.hs

--- Human Readable Time.hs
+++ Human Readable Time.hs
@@ -6,17 +6,17 @@
 -- The maximum time never exceeds 359999 (99:59:59)
 
 humanReadable :: Int -> String
-humanReadable x = show hours ++ ":" ++ show minutes ++ ":" ++ show seconds
+humanReadable x
+  | x <= 59 = "00:00:" ++ to2Digits x
+  | otherwise = evalMinutes x
   where
-    hours
-      | (x `div` 60 - 59) `div` 60 > 99 = 99
-      | otherwise = (x `div` 60 - 59) `div` 60
-    minutes
-      | x `div` 60 > 59 = 59
-      | otherwise = x `div` 60
-    seconds
-      | x `rem` 60 > 59 = 59
-      | otherwise = x `rem` 60
+    to2Digits n
+      | n < 10 = "0" ++ show n
+      | otherwise = show n
+    evalMinutes s
+      | (s `div` 60) <= 59 = "00:" ++ to2Digits (s `div` 60) ++ ":" ++ to2Digits (s `rem` 60)
+      | otherwise = evalHours s
+    evalHours h = to2Digits (((h `div` 60) - 59) `div` 60) ++ to2Digits (h `div` 60) ++ ":" ++ to2Digits (h `rem` 60)
 
 main = do
   print $ humanReadable 0 -- " should be 00:00:00"
@@ -25,3 +25,4 @@ main = do
   print $ humanReadable 90 -- " should be 00:01:30"
   print $ humanReadable 86399 -- " should be 23:59:59"
   print $ humanReadable 359999 -- " should be 99:59:59"
+  print $ humanReadable 324953 -- " should be 90:15:53"
