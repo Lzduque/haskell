75d513f76e9ac07d98ae6a7715e9dba9626f649b
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-01-15 13:24:09 -0500

    Update Number Line Jumps.hs

--- Number Line Jumps.hs
+++ Number Line Jumps.hs
@@ -4,13 +4,26 @@
 --  2. INTEGER v1
 --  3. INTEGER x2
 --  4. INTEGER v2
+-- they have to get to the same place at the same time
+-- x1 + (v1 * y) = x2 + (v2 * y)
+-- x1 + v1*y = x2 + v2*y
+-- x1 - x2 = v2*y - v1*y 
+-- v2*y - v1*y = x1 - x2 
+-- y (v2 - v1) = x1 - x2 
+-- y = (x1 - x2) / (v2 - v1)
+-- y has to be a round number
 
--- Integer -> Integer -> Integer -> Integer ->String
-kangaroo x1 v1 x2 v2 = do
-    -- Write your code here
+kangaroo :: Integer -> Integer -> Integer -> Integer -> String
+kangaroo x1 v1 x2 v2
+  | y == fromIntegral (round y) = "YES"
+  | otherwise = "NO"
+  where
+    y :: Float
+    y = fromIntegral (x1 - x2) / fromIntegral (v2 - v1)
+
 
 
 main = do
-  print $ show $ kangaroo 2 1 1 2 -- should be 'YES'
-  print $ show $ kangaroo 0 3 4 2 -- should be 'YES'
-  print $ show $ kangaroo 0 2 5 3 -- should be 'NO'
+  print (kangaroo 2 1 1 2) -- should be 'YES'
+  print (kangaroo 0 3 4 2) -- should be 'YES'
+  print (kangaroo 0 2 5 3) -- should be 'NO'
