3e4fc39d971b636c6f54448fa5c7ed234242c4a0
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-01-24 11:19:29 -0500

    Create Divisible Sum Pairs.hs

---
+++ Divisible Sum Pairs.hs
@@ -0,0 +1,9 @@
+-- as a human I would approach like:
+-- 
+
+divisibleSumPairs n k ar = do
+    -- Write your code here
+
+main = do
+ print (divisibleSumPairs 6 3 [1, 3, 2, 6, 1, 2]) -- should be '5'
+ print "should be 5"  
