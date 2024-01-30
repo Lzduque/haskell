341e347a60cd40e80d77e0020f43210b63c0f9bc
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-01-30 13:54:51 -0500

    Update Day of the Programmer.hs

--- Day of the Programmer.hs
+++ Day of the Programmer.hs
@@ -1,5 +1,29 @@
 dayOfProgrammer :: Int -> String
 dayOfProgrammer year
+ | isJulianYear year = yearJulian year
+ | year == 1918 = "26.09.1918"
+ | otherwise = yearGregorian year
+
+isJulianYear :: Int -> Bool
+isJulianYear year
+ | year <= 1917 = True
+ | otherwise = False
+
+yearJulian :: Int -> String
+yearJulian year
+ | year `rem` 4 == 0 = "12.09." ++ (intToString year)
+ | otherwise =  "13.09." ++ (intToString year)
+--  | otherwise =  "normal julian" 
+
+yearGregorian :: Int -> String
+yearGregorian year
+ | year `rem` 400 == 0 = "12.09." ++ (intToString year)
+ | year `rem` 4 == 0 && year `rem` 100 /= 0 =  "12.09." ++ (intToString year)
+ | otherwise =  "13.09." ++ (intToString year)
+--  | otherwise =  "normal gregorian" 
+
+intToString :: Int -> String
+intToString n = show n
 
 main = do
  print (dayOfProgrammer 2017) -- should be '13.09.2017'
