11da32451da8eaa39c616ae0434f7876994d9b4b
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-01-31 10:52:30 -0500

    Update Bill Division.hs

--- Bill Division.hs
+++ Bill Division.hs
@@ -1,12 +1,20 @@
-bonAppetit :: [Int] -> Int -> Int -> Either String Int
+-- this is how it should be done:
+-- bonAppetit :: [Int] -> Int -> Int -> Either String Int
+-- bonAppetit bill k ana = if ana == trueBill
+--  then Left "Bon Appetit"
+--  else Right (ana - trueBill)
+--  where
+--   trueBill = ((sum bill) - bill!!k) `div` 2
+
+bonAppetit :: [Int] -> Int -> Int -> IO ()
 bonAppetit bill k ana = if ana == trueBill
- then Left "Bon Appetit"
- else Right (ana - trueBill)
+ then putStrLn $ id "Bon Appetit"
+ else print (ana - trueBill)
  where
   trueBill = ((sum bill) - bill!!k) `div` 2
 
 main = do
- print (bonAppetit [3,10,2,9] 1 12) -- should be '5'
+ bonAppetit [3,10,2,9] 1 12 -- should be '5'
  print "should be 5"
- print (bonAppetit [3,10,2,9] 1 7) -- should be 'Bon Appetit'
+ bonAppetit [3,10,2,9] 1 7 -- should be 'Bon Appetit'
  print "should be Bon Appetit"
