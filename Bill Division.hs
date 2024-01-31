76fcae94840cae7f7b5dd75fda53359aa2fa3c67
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-01-31 10:43:50 -0500

    Update Bill Division.hs

--- Bill Division.hs
+++ Bill Division.hs
@@ -1,6 +1,9 @@
-bonAppetit :: [Int] -> Int -> Int -> String
-bonAppetit bill k ana = do
-    -- Write your code here
+bonAppetit :: [Int] -> Int -> Int -> Either String Int
+bonAppetit bill k ana = if ana == trueBill
+ then Left "Bon Appetit"
+ else Right (ana - trueBill)
+ where
+  trueBill = ((sum bill) - bill!!k) `div` 2
 
 main = do
  print (bonAppetit [3,10,2,9] 1 12) -- should be '5'
