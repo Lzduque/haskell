88cf2a8cbf18d5ebebf547263bf40d6b024caed7
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-01 09:55:33 -0500

    Update Drawing Book.hs

--- Drawing Book.hs
+++ Drawing Book.hs
@@ -1,5 +1,14 @@
 pageCount :: Int -> Int -> Int
 pageCount n p
+ | pagesFromFront n p > pagesFromBack n p = pagesFromFront n p
+ | otherwise = pagesFromBack n p
+
+pagesFromFront :: Int -> Int -> Int
+pagesFromFront n p = ceiling ((p - 1) `div` 2)
+
+pagesFromBack :: Int -> Int -> Int
+pagesFromBack n p = ceiling ((n - p) `div` 2)
+
 main = do
   print (pageCount 6 2) -- should be '1'
   print "should be 1"
