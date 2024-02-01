68193864ae8ac2613a8ff65ad3a5323c5c989baa
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-01 13:16:42 -0500

    Update Drawing Book.hs

--- Drawing Book.hs
+++ Drawing Book.hs
@@ -1,7 +1,10 @@
 pageCount :: Int -> Int -> Int
 pageCount n p
- | pagesFromFront n p > pagesFromBack n p = pagesFromBack n p
- | otherwise = pagesFromFront n p
+ | distFront > distback = distback
+ | otherwise = distFront
+ where
+  distFront = pagesFromFront n p
+  distback = pagesFromBack n p
 
 pagesFromFront :: Int -> Int -> Int
 pagesFromFront n p = ceiling ((p' - 1) / 2)
@@ -10,18 +13,24 @@ pagesFromFront n p = ceiling ((p' - 1) / 2)
 
 pagesFromBack :: Int -> Int -> Int
 pagesFromBack n p
- | (n `rem` 2) /= 0 = floor ((n' - p') / 2)
+ | isOdd n = floor ((n' - p') / 2)
  | otherwise = ceiling ((n' - p') / 2)
   where
    p' = fromIntegral p
    n' = fromIntegral n
+   isOdd a = (a `rem` 2) /= 0
+
 
 main = do
-  print (pageCount 6 2) -- should be '1'
+  print (pageCount 6 2)
   print "should be 1"
-  print (pageCount 5 3) -- should be '1'
+  print (pageCount 6 2 == 1)
+  print (pageCount 5 3)
   print "should be 1"
-  print (pageCount 6 4) -- should be '1'
+  print (pageCount 5 3 == 1)
+  print (pageCount 6 4)
   print "should be 1"
-  print (pageCount 5 4) -- should be '0'
+  print (pageCount 6 4 == 1)
+  print (pageCount 5 4)
   print "should be 0"
+  print (pageCount 5 4 == 0)
