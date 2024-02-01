f8214719a614f0e30f7b9bd28853c81986c1878e
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-01 12:36:29 -0500

    Update Drawing Book.hs

--- Drawing Book.hs
+++ Drawing Book.hs
@@ -1,25 +1,27 @@
 pageCount :: Int -> Int -> Int
 pageCount n p
- | pagesFromFront n' p' > pagesFromBack n' p' = pagesFromBack n' p'
- | otherwise = pagesFromFront n' p'
+ | pagesFromFront n p > pagesFromBack n p = pagesFromBack n p
+ | otherwise = pagesFromFront n p
+
+pagesFromFront :: Int -> Int -> Int
+pagesFromFront n p = ceiling ((p' - 1) / 2)
  where
-  p' = fromIntegral p
-  n'
-   | n `rem` 2 == 0 = fromIntegral (n + 1)
-   | otherwise = fromIntegral n
+   p' = fromIntegral p
 
-pagesFromFront :: (RealFrac a, Num a) => a -> a -> Int
-pagesFromFront n p = ceiling ((p - 1) / 2)
-
-pagesFromBack :: (RealFrac a, Num a) => a -> a -> Int
-pagesFromBack n p = ceiling ((n - p) / 2)
+pagesFromBack :: Int -> Int -> Int
+pagesFromBack n p
+ | (n `rem` 2) /= 0 = floor ((n' - p') / 2)
+ | otherwise = ceiling ((n' - p') / 2)
+  where
+   p' = fromIntegral p
+   n' = fromIntegral n
 
 main = do
   print (pageCount 6 2) -- should be '1'
   print "should be 1"
   print (pageCount 5 3) -- should be '1'
   print "should be 1"
-  print (pageCount 5 4) -- should be '0'
-  print "should be 0"
   print (pageCount 6 4) -- should be '1'
   print "should be 1"
+  print (pageCount 5 4) -- should be '0'
+  print "should be 0"
