7ffaf10e3a2f00ad203d381a378fff89198031ca
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-02 12:07:25 -0500

    Update Drawing Book.hs

--- Drawing Book.hs
+++ Drawing Book.hs
@@ -1,24 +1,16 @@
 pageCount :: Int -> Int -> Int
-pageCount n p
- | distFront > distback = distback
- | otherwise = distFront
- where
-  distFront = pagesFromFront n p
-  distback = pagesFromBack n p
-
-pagesFromFront :: Int -> Int -> Int
-pagesFromFront n p = ceiling ((p' - 1) / 2)
- where
-   p' = fromIntegral p
-
-pagesFromBack :: Int -> Int -> Int
-pagesFromBack n p
- | isOdd n = floor ((n' - p') / 2)
- | otherwise = ceiling ((n' - p') / 2)
+pageCount n p = min distFront distBack
   where
-   p' = fromIntegral p
-   n' = fromIntegral n
-   isOdd a = (a `rem` 2) /= 0
+    distFront = ceiling ((p' - firstPage) / 2) 
+    distBack
+     | odd n = floor ((n' - p') / 2)
+     | otherwise = ceiling ((n' - p') / 2)
+    firstPage = 0
+    lastPage
+      | odd n = n
+      | otherwise = n +1
+    p' = fromIntegral p
+    n' = fromIntegral n
 
 
 main = do
