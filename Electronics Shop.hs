9f9f624bc39b182a4356c8d73d7282c622025a0f
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-07 13:35:33 -0500

    Update Electronics Shop.hs

--- Electronics Shop.hs
+++ Electronics Shop.hs
@@ -3,12 +3,13 @@
 -- is it larger than the last value AND smaller or euqal to the budget? The first default value is -1, so if nothing matches, that would be the answer.
 
 getMoneySpent :: [Int] -> [Int] -> Int -> Int
-getMoneySpent keyboards drives b = foldl compareKeyboards -1 keyboards
+getMoneySpent keyboards drives b = foldl compareKeyboards (-1) keyboards
   where
     compareKeyboards acc cur = foldl compareDrives acc drives
-    compareDrives x y
-      | cur + y <= b && cur + y > acc = cur + y
-      | otherwise = x
+      where
+        compareDrives x y
+          | cur + y <= b && cur + y > acc = cur + y
+          | otherwise = x
 
 main = do
   print (getMoneySpent [3,1] [5,2,8] 9)
