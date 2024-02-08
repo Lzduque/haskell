ab2357ee2ece9c2448dce89c7b45c42166682c78
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-08 13:39:58 -0500

    Update Viral Advertising.hs

--- Viral Advertising.hs
+++ Viral Advertising.hs
@@ -1,5 +1,8 @@
 viralAdvertising :: Int -> Int
-    
+viralAdvertising n = (foldl likes 5 days)
+  where
+    days = [1 .. n]
+    likes acc cur = (floor ((fromIntegral acc) / 2)) * (3)
 
 
 main = do
@@ -9,3 +12,6 @@ main = do
   print (viralAdvertising 3)
   print "should be 9"
   print ((viralAdvertising 3) == 9)
+  print (viralAdvertising 50)
+  print "should be 2068789129"
+  print ((viralAdvertising 50) == 2068789129)
