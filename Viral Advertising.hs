3c775d26ce536fa98f50f3cdcbaee033ec625af0
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2024-02-08 12:33:11 -0500

    Create Viral Advertising.hs

---
+++ Viral Advertising.hs
@@ -0,0 +1,11 @@
+viralAdvertising :: Int -> Int
+    
+
+
+main = do
+  print (viralAdvertising 5)
+  print "should be 24"
+  print ((viralAdvertising 5) == 24)
+  print (viralAdvertising 3)
+  print "should be 9"
+  print ((viralAdvertising 3) == 9)
